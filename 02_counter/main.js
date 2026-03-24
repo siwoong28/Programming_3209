13669d4b4504555187faf52c965bfbc79389f524
siwoong <siwoong28@gmail.com>
2026-03-24 09:53:00 +0900

    counter HTML onclick

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -4,16 +4,16 @@
 // const resultH1 = document.getElementById("num");
 // const resultH1 = document.querySelectorAll("h1")[0];
 
-const resultH1 = document.getElementsByTagName("h1")[0];
-const plusButton = document.getElementsByTagName("button")[0];
 let count = 0;
+const resultH1 = document.getElementsByTagName("h1")[0];
 // const resultH1 = document.getElementById("result");
 // const plusButton = document.getElementsByClassName("plusBtn")[0];
 // plusButton.addEventListener("click",() => {
 //     count++;
 //     resultH1.innerHTML = count;
 // });
-plusButton.onclick = () => {
+
+function plus(){
     count++;
     resultH1.innerHTML = count;
-};
+}
