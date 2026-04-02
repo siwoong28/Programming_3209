177be711b11b33d57b3ab5f2cc28de8543f387d6
siwoong <siwoong28@gmail.com>
2026-04-02 13:28:56 +0900

    CounterApp plusMax10

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -8,21 +8,31 @@ function CounterApp() {
   //count: 이전 또는 현재 값
   //setCount():count값 변경 함수. 반드시 이걸 거쳐야 함
   const [count, setCount] = useState(0)
+  function plusMax10() {
+    setCount((count) => {
+      if (count + 1 > 10) return 10;
+      return count + 1;
+    })
+  }
 
   return (
     <>
       <h1>{count}</h1>
-    <button onClick={() => setCount((count) => count + 1)}>+</button>
-    <button onClick={() => setCount((count) => count + 2)}>+2</button>
-    <button onClick={() => setCount((count) => count - 1)}>-</button>
-    <button onClick={() => setCount(0)}>reset</button>
-    <button onClick={() => setCount((count) => {
-      if(count+1>10) return 10;
-        return count+1;
-    })}>+(최대 10까지)</button>
-    <button onClick={() => setCount((count) => count+1 >= 10 ? 10 : count+1)}>+(최대 10까지)2</button>
-    <button onClick={() => setCount((count) => Math.min(count+1,10))}>+(최대 10까지)3</button>
-    {/* <button onClick={setCount(0)}>reset</button> Too many re-renders  무한반복이 돈다.*/}
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      <button onClick={() => setCount((count) => count + 2)}>+2</button>
+      <button onClick={() => setCount((count) => count - 1)}>-</button>
+      <button onClick={() => setCount(0)}>reset</button>
+      <button onClick={() => setCount((count) => {
+        if (count + 1 > 10) return 10;
+        return count + 1;
+      })}>+(최대 10까지)</button>
+      <button onClick={() => setCount((count) => count + 1 >= 10 ? 10 : count + 1)}>+(최대 10까지)2</button>
+      <button onClick={() => setCount((count) => Math.min(count + 1, 10))}>+(최대 10까지)3</button>
+
+      <button onClick={() => plusMax10()}>+(최대 10까지)</button>
+      <button onClick={plusMax10}>+(최대 10까지)</button>
+
+      {/* <button onClick={setCount(0)}>reset</button> Too many re-renders  무한반복이 돈다.*/}
       {/* <button
         className="counter"
         onClick={() => setCount((count) => count + 1)} //JS 함수호출X 함수정의O
