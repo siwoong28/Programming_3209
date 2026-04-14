8f0601edda0d2413591527ecce725a5b059e1167
siwoong <siwoong28@gmail.com>
2026-04-14 10:11:19 +0900

    TodoLIstApp addTodo()

--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -6,7 +6,9 @@ export default function TodoAdder({ addTodo }) { //    const { addTodo } = props
     const [inputTodo, setInputTodo] = useState('');
     const handleSubmit = (event) => {
         event.preventDefault(); //기본 동작 막자
-        addTodo(inputTodo); //input에 있는 text value를 할일 내용으로 추가하자
+        if(!inputTodo.trim()) return; //빈칸이면 바로 return
+        addTodo(inputTodo.trim()); //input에 있는 text value를 할일 내용으로 추가하자
+        setInputTodo('');
     }
     return (
         <form action="" className='todo__form' onSubmit={handleSubmit}>
